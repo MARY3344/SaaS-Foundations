d0c2e5ee7fd7e00c452d00fc0a64e21d0dbcf516
Mary <mary.jose@simelabs.com>
2025-08-03 14:30:43 +0530

    database neon added

--- SaaS-Foundations-section-1/src/cfehome/wsgi.py
+++ SaaS-Foundations-section-1/src/cfehome/wsgi.py
@@ -7,15 +7,15 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
-import os
-import django
-from django.core.management import call_command
+# import os#error fix
+# import django#error fix
+# from django.core.management import call_command ##error fix
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cfehome.settings')
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cfehome.settings') #error fix
 
-django.setup()
-call_command('migrate')
+# django.setup()#error fix
+# call_command('migrate')#error fix
 
 application = get_wsgi_application()
 
