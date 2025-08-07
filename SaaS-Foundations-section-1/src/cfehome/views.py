685f6420434968b8cea7690bd9539ab3274a9a8b
Mary <mary.jose@simelabs.com>
2025-08-07 11:12:51 +0530

    Django login view, HTML Login Form and Template

--- SaaS-Foundations-section-1/src/cfehome/views.py
+++ SaaS-Foundations-section-1/src/cfehome/views.py
@@ -7,7 +7,11 @@ from visits.models import PageVisit
 this_dir = pathlib.Path(__file__).resolve().parent
 
 def home_view(request, *args, **kwargs):
+    if request.user.is_authenticated:
+        print(request.user.first_name)
+        print("+======")
     return about_view(request, *args, **kwargs)
+
 
 
 def about_view(request, *args, **kwargs):
